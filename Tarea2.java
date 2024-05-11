b8ea1c0c538bb64a73201ba5a3890b6e98beb69e
kiryu-victor <kiryu.henki@gmail.com>
2024-05-11 12:20:22 +0100

    Primer commit de la tarea 2

---
+++ Tarea2.java
@@ -0,0 +1,12 @@
+package tarea2;
+
+/**
+ *
+ * @author Víctor Galán
+ */
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        System.out.println("Primer commit de la tarea 2");
+    }    
+}
